93c08949bdc2a9f8d39b6d172c279dbb5daa5a31
Anna <anickab99@gmail.com>
2025-04-01 20:10:43 +0200

    rybicka opeplave kamen do ctverce pomoci 4 cyklu for

--- src/main/java/cz/czechitas/java/lekce01/RybkaApplication.java
+++ src/main/java/cz/czechitas/java/lekce01/RybkaApplication.java
@@ -8,33 +8,45 @@ import cz.czechitas.java.playground.Rybka;
 import javafx.scene.paint.Color;
 
 public class RybkaApplication extends CzechitasGameApplication {
-  private Rybka rybka;
-  private Kamen kamen;
+    private Rybka rybka;
+    private Kamen kamen;
 
-  public static void main(String[] args) {
-    launch(args);
-  }
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-  @Override
-  protected void initSettings(GameSettings settings) {
-    settings.setNative(true);
-    settings.setWidth(800);
-    settings.setHeight(600);
-    settings.setTitle("Rybka Renata");
-    settings.setVersion("1.0");
-  }
+    @Override
+    protected void initSettings(GameSettings settings) {
+        settings.setNative(true);
+        settings.setWidth(800);
+        settings.setHeight(600);
+        settings.setTitle("Rybka Renata");
+        settings.setVersion("1.0");
+    }
 
-  @Override
-  protected void initGame() {
-    FXGL.getGameScene().setBackgroundColor(Color.AQUA);
-    super.initGame();
-  }
+    @Override
+    protected void initGame() {
+        FXGL.getGameScene().setBackgroundColor(Color.AQUA);
+        super.initGame();
+    }
 
-  @Override
-  protected void onStart() {
-    kamen = new Kamen(200, 200);
-    rybka = new Rybka(100, 100);
+    @Override
+    protected void onStart() {
+        kamen = new Kamen(200, 200);
+        rybka = new Rybka(100, 100);
 
-    //TODO tady napište svůj kód
-  }
+        //TODO tady napište svůj kód
+        for (int i = 0; i < 20; i++) {
+            rybka.plavatVpravo();
+        }
+        for (int i = 0; i < 20; i++) {
+            rybka.plavatDolu();
+        }
+        for (int i = 0; i < 20; i++) {
+            rybka.plavatVlevo();
+        }
+        for (int i = 0; i < 20; i++) {
+            rybka.plavatNahoru();
+        }
+    }
 }
